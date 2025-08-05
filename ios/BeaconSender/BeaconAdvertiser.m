59274b3027d28a2a91fa0c2b4f150b68a8e5b48a
Bertho <berthoazeye21@gmail.com>
2025-08-05 16:45:15 +0100

    edit-projet-name

--- ios/BeaconSender/BeaconAdvertiser.m
+++ ios/BeaconSender/BeaconAdvertiser.m
@@ -1,15 +1,19 @@
 #import <React/RCTBridgeModule.h>
 
-@interface RCT_EXTERN_MODULE(BeaconAdvertiser, NSObject)
+@interface RCT_EXTERN_MODULE(BeaconAdvertiserModule, NSObject)
 
-RCT_EXTERN_METHOD(startIBeacon:(NSString *)uuidString 
-                  major:(NSNumber *)major 
-                  minor:(NSNumber *)minor 
-                  txPower:(NSNumber *)txPower 
-                  resolver:(RCTPromiseResolveBlock)resolve 
-                  rejecter:(RCTPromiseRejectBlock)reject)
+RCT_EXTERN_METHOD(
+  startIBeacon:(NSString *)uuidStr
+  major:(nonnull NSNumber *)major
+  minor:(nonnull NSNumber *)minor
+  txPower:(nonnull NSNumber *)txPower
+  withResolver:(RCTPromiseResolveBlock)resolve
+  withRejecter:(RCTPromiseRejectBlock)reject
+)
 
-RCT_EXTERN_METHOD(stopAdvertising:(RCTPromiseResolveBlock)resolve 
-                  rejecter:(RCTPromiseRejectBlock)reject)
+RCT_EXTERN_METHOD(
+  stopAdvertising:(RCTPromiseResolveBlock)resolve
+  withRejecter:(RCTPromiseRejectBlock)reject
+)
 
 @end
